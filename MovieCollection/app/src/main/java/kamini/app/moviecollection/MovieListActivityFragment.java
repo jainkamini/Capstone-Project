32cd28e8f992046954f9cd1de8f7609ddc5b21d6
KAMINI <jainkamini08@gmail.com>
2016-03-12 11:33:42 -0600

    add dependencies in gradle

---
+++ MovieCollection/app/src/main/java/kamini/app/moviecollection/MovieListActivityFragment.java
@@ -0,0 +1,23 @@
+package kamini.app.moviecollection;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MovieListActivityFragment extends Fragment {
+
+    public MovieListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movie_list, container, false);
+    }
+}
