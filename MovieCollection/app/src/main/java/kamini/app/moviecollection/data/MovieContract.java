16a671911e82aac5a555babb324131de495ac918
KAMINI <jainkamini08@gmail.com>
2016-03-12 13:19:43 -0600

    implement local database

---
+++ MovieCollection/app/src/main/java/kamini/app/moviecollection/data/MovieContract.java
@@ -0,0 +1,106 @@
+package kamini.app.moviecollection.data;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Kamini on 9/28/2015.
+ */
+public class MovieContract {
+
+
+
+
+    // The "Content authority" is a name for the entire content provider, similar to the
+    // relationship between a domain name and its website.  A convenient string to use for the
+    // content authority is the package name for the app, which is guaranteed to be unique on the
+    // device.
+    public static final String CONTENT_AUTHORITY = "kamini.app.moviecollection";
+
+    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+    // the content provider.
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_MOVIE = "movie";
+    public static final String PATH_TRAILER = "trailer";
+    public static final String PATH_REVIEW = "review";
+
+    /* Inner class that defines the table contents of the movie table */
+    public static final class MovieEntry implements BaseColumns {
+
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+
+        // Table name
+        public static final String TABLE_NAME = "movie_details";
+
+
+
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+        public static final String COLUMN_MOVIE_POSTER = "movie_poster";
+        public static final String COLUMN_MOVIE_TITLE = "movie_title";
+        public static final String COLUMN_MOVIE_DATE = "movie_date";
+        public static final String COLUMN_MOVIE_RATING = "movie_rating";
+        public static final String COLUMN_MOVIE_OVERVIEW = "movie_overview";
+
+
+        public static Uri buildMovieUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+
+
+    }
+    public static final class TrailerEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_TRAILER).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TRAILER;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TRAILER;
+        // Table name
+        public static final String TABLE_NAME = "trailer";
+
+
+
+        public static final String COLUMN_MOVIEID_KEY = "movie_id";
+        public static final String COLUMN_MOVIE_KEY = "trailer_key";
+        public static final String COLUMN_TRAILER_NMAE = "trailer_name";
+
+        public static Uri buildTrailerUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+
+    }
+
+    public static final class ReviewEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_REVIEW).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_REVIEW;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_REVIEW;
+
+        // Table name
+        public static final String TABLE_NAME = "review";
+        // Column with the foreign key into the  table.
+        public static final String COLUMN_MOVIEID_KEY = "movie_id";
+
+        public static final String COLUMN_MOVIE_AUTHOR = "review_poster";
+        public static final String COLUMN_MOVIE_CONTENT = "review_content";
+        public static Uri buildReviewUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+}
---
+++ MovieCollection/app/src/main/java/kamini/app/moviecollection/data/MovieDbHelper.java
@@ -0,0 +1,94 @@
+package kamini.app.moviecollection.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import kamini.app.moviecollection.data.MovieContract.MovieEntry;
+import kamini.app.moviecollection.data.MovieContract.TrailerEntry;
+import kamini.app.moviecollection.data.MovieContract.ReviewEntry;
+
+
+/**
+ * Manages a local database for movie data.
+ */
+public class MovieDbHelper extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    private static final int DATABASE_VERSION = 2;
+
+    static final String DATABASE_NAME = "movie.db";
+    public MovieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
+                MovieEntry._ID + " INTEGER PRIMARY KEY," +
+                MovieEntry.COLUMN_MOVIE_ID +" INTEGER NOT NULL , " +
+               MovieEntry.COLUMN_MOVIE_DATE +" TEXT   , " +
+                MovieEntry.COLUMN_MOVIE_POSTER +" TEXT NOT NULL , " +
+               MovieEntry.COLUMN_MOVIE_OVERVIEW +" TEXT NOT NULL , " +
+               MovieEntry.COLUMN_MOVIE_RATING +" TEXT NOT NULL , " +
+                MovieEntry.COLUMN_MOVIE_TITLE +" TEXT NOT NULL  " +
+
+
+        " );";
+
+        final String SQL_CREATE_TRAILER_TABLE = "CREATE TABLE " + MovieContract.TrailerEntry.TABLE_NAME + " (" +
+                // Why AutoIncrement here, and not above?
+                // Unique keys will be auto-generated in either case.  But for weather
+                // forecasting, it's reasonable to assume the user will want information
+                // for a certain date and all dates *following*, so the forecast data
+                // should be sorted accordingly.
+                TrailerEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+
+                // the ID of the location entry associated with this weather data
+                TrailerEntry.COLUMN_MOVIEID_KEY + " INTEGER NOT NULL, " +
+                TrailerEntry.COLUMN_MOVIE_KEY + " TEXT NOT NULL, " +
+               TrailerEntry.COLUMN_TRAILER_NMAE + " TEXT NOT NULL, " +
+
+
+
+
+                // Set up the location column as a foreign key to location table.
+                " FOREIGN KEY (" + TrailerEntry.COLUMN_MOVIEID_KEY+ ") REFERENCES " +
+                MovieEntry.TABLE_NAME + " (" + MovieEntry._ID + ") " +
+
+               ");";
+
+        final String SQL_CREATE_REVIEW_TABLE = "CREATE TABLE " + ReviewEntry.TABLE_NAME + " (" +
+                // Why AutoIncrement here, and not above?
+                // Unique keys will be auto-generated in either case.  But for weather
+                // forecasting, it's reasonable to assume the user will want information
+                // for a certain date and all dates *following*, so the forecast data
+                // should be sorted accordingly.
+                ReviewEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+
+                // the ID of the location entry associated with this weather data
+                ReviewEntry.COLUMN_MOVIEID_KEY + " INTEGER NOT NULL, " +
+                ReviewEntry.COLUMN_MOVIE_AUTHOR + " TEXT NOT NULL, " +
+                ReviewEntry.COLUMN_MOVIE_CONTENT + " TEXT NOT NULL, " +
+                // Set up the location column as a foreign key to location table.
+                " FOREIGN KEY (" + TrailerEntry.COLUMN_MOVIEID_KEY+ ") REFERENCES " +
+                MovieEntry.TABLE_NAME + " (" + MovieEntry._ID + ") " +
+
+                ");";
+        sqLiteDatabase.execSQL(SQL_CREATE_MOVIE_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_TRAILER_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_REVIEW_TABLE);
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        // Note that this only fires if you change the version number for your database.
+        // It does NOT depend on the version number for your application.
+        // If you want to update the schema without wiping data, commenting out the next 2 lines
+        // should be your top priority before modifying this method.
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TrailerEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ReviewEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+}
